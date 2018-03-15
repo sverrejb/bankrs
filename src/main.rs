5438a4d8cd6df3dc8d1501e63f80db424ea15a29
Sverre Johann Bjørke <sverrejb@gmail.com>
2018-03-15 19:37:28 +0100

    started on args config

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,26 @@
+#[macro_use]
+
 extern crate clap;
-use clap::{Arg, App, SubCommand};
+use clap::{Arg, App};
 
 fn main() {
-    let matches = App::new("CLI-bank")
-        .version("1.0")
-        .author("Sverre Johann Bjørke <sverrejb@gmail.com>")
+    let matches = App::new("bankrs")
+        .version(crate_version!())
+        .author(crate_authors!())
         .about("Check balance and transactions")
         .arg(Arg::with_name("balance")
             .short("b")
             .long("balance")
-            .help("Check balance")
+            .help("Check balance"))
+        .arg(Arg::with_name("number of transactions")
+            .short("t")
+            .long("transactions")
+            .help("Lists recent transactions")
             .takes_value(true))
-        .arg(Arg::with_name("v")
-            .short("v")
-            .multiple(true)
-            .help("Sets the level of verbosity"))
+        .arg(Arg::with_name("bank").help("Specify which bank to check"))
         .get_matches();
 
-    // Gets a value for config if supplied by user, or defaults to "default.conf"
-    let config = matches.value_of("config").unwrap_or("default.conf");
-    println!("Value for config: {}", config);
 
-    // Calling .unwrap() is safe here because "INPUT" is required (if "INPUT" wasn't
-    // required we could have used an 'if let' to conditionally get the value)
-    println!("Using input file: {}", matches.value_of("INPUT").unwrap());
-
-
-
-    // You can handle information about subcommands by requesting their matches by name
-    // (as below), requesting just the name used, or both at the same time
-    if let Some(matches) = matches.subcommand_matches("test") {
-        if matches.is_present("debug") {
-            println!("Printing debug info...");
-        } else {
-            println!("Printing normally...");
-        }
     }
 
     // more program logic goes here...
-}
