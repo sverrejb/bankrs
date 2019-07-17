d4ac6d108dbc95e86c059c0366b28386bcae4fb1
Sverre Johann Bjørke <sverrejb@gmail.com>
2019-07-17 22:53:47 +0200

    working get-request to dummy api

--- src/main.rs
+++ src/main.rs
@@ -3,36 +3,41 @@
 extern crate clap;
 extern crate banklib;
 
-use clap::{Arg, App};
-use banklib::get_transactions;
 use banklib::get_balance;
-
+use banklib::get_transactions;
+use clap::{App, Arg};
 
 fn main() {
     let matches = App::new("bankrs")
         .version(crate_version!())
         .author(crate_authors!())
         .about("Check balance and transactions")
-        .arg(Arg::with_name("balance")
-            .short("b")
-            .long("balance")
-            .takes_value(false)
-            .help("Check balance"))
-        .arg(Arg::with_name("number of transactions")
-            .short("t")
-            .long("transactions")
-            .help("Lists n recent transactions")
-            .takes_value(true))
+        .arg(
+            Arg::with_name("balance")
+                .short("b")
+                .long("balance")
+                .takes_value(false)
+                .help("Check balance"),
+        )
+        .arg(
+            Arg::with_name("n")
+                .short("t")
+                .long("transactions")
+                .help("Lists n recent transactions")
+                .takes_value(true),
+        )
         .arg(Arg::with_name("bank"))
         .get_matches();
 
-    if matches.is_present("balance"){
-        println!("{}", get_balance());
+    if matches.is_present("balance") {
+        let balance = match get_balance() {
+            Ok(v) => v,
+            Err(e) => panic!("Unable to get balance: {:?}", e)
+        };
+        println!("{}", balance)
     }
 
     if let Some(number_of_transactions) = matches.value_of("number of transactions") {
         println!("{}", get_transactions(number_of_transactions));
     }
-
-
-    }
+}
